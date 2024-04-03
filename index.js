df1d936f553dbcabe021593ade1fd8aca5c73696
Jacob <jacobpayne007@gmail.com>
2024-04-02 20:32:36 -0500

    assignment 2 done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
